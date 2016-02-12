17ea2622156a323a94434b2dd4ed8e527b34aa3f
Miguel Briceño <mbriceno@4geeks.co>
2016-02-12 09:43:43 -0430

    Cambios para electron

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,4 @@
 import {bootstrap}    from 'angular2/platform/browser'
 import {LibroRecetasComponents} from './app.component'
 
-//import {FooterComponent} from './footer.component'
-
 bootstrap(LibroRecetasComponents);
